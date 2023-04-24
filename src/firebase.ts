727e42aef55ceccbfd2fcac76251adb9f3a5fcf4
Alexander Alexeev <alekseev@trueconf.ru>
2023-04-24 22:33:52 +0200

    add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp, FirebaseApp, FirebaseOptions } from 'firebase/app';
+import { getAuth, Auth } from 'firebase/auth';
+import { getDatabase, Database } from 'firebase/database';
+import { getStorage, FirebaseStorage } from 'firebase/storage';
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  databaseURL: process.env.FB_DATABASE_URL,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEASUREMENT_ID,
+};
+
+const firebaseApp: FirebaseApp = initializeApp(firebaseConfig);
+
+export const firebaseAuth: Auth = getAuth(firebaseApp);
+export const firebaseDataBase: Database = getDatabase(firebaseApp);
+export const firebaseStorage: FirebaseStorage = getStorage(firebaseApp);
