090d17ebcb34b84fd710289b6716c33e2fac348e
Alexander Alexeev <alekseev@trueconf.ru>
2023-07-02 16:03:04 +0200

    edit to use header conteiners

--- src/components/views/Skills/Skills.tsx
+++ src/components/views/Skills/Skills.tsx
@@ -7,6 +7,7 @@ import { StaticProps } from '@/shared/types/StaticProps.type';
 import { ISkill } from 'my-portfolio-types';
 import { useRouter } from 'next/router';
 import { ProtectedRoutePath } from '@/router/Routes.enum';
+import BaseContentHeaderContainer from '@/components/UI/Containers/BaseContentHeader.container';
 
 const Skills: FC<StaticProps<Array<ISkill>>> = ({payload}): ReactElement => {
   const router = useRouter();
@@ -16,67 +17,71 @@ const Skills: FC<StaticProps<Array<ISkill>>> = ({payload}): ReactElement => {
   };
 
   return (
-    <BaseContentContainer>
-      {
-        payload.length && <Stack direction={'row'} alignItems={'start'} justifyContent={'end'} w={'full'} spacing={4}>
-          <Button colorScheme={'teal'} onClick={() => prepareCreateSkill()}>Create skill</Button>
-
-          <Button colorScheme={'red'}>Remove all skills</Button>
-        </Stack>
-      }
-
-      {
-        payload.length ?
-          payload.map((skillCard) => (
-          <Card
-            key={skillCard.id}
-            direction={{ base: 'column', sm: 'row' }}
-            overflow={'hidden'}
-            variant={'outline'}
-            w={'full'}>
-            <Stack direction={'column'} w={'full'} overflow={'hidden'}>
-              <CardBody p={4}>
-                <Stack direction={'column'} w={'full'} spacing={4} overflow={'hidden'}>
-                  <Stack direction={'row'} spacing={4}>
-                    <Tooltip label={'Skill visibility'} aria-label={'skill visibility'}>
-                      <Icon color={skillCard.visibility ? 'teal.500' : 'red.500'} as={skillCard.visibility ? VisibilityIcon : VisibilityOffIcon}/>
-                    </Tooltip>
-
-                    <Heading size={'md'}>{skillCard.title}</Heading>
-                  </Stack>
-
-                  <Stack direction={'row'} alignItems={'center'} justifyContent={'start'} spacing={2} overflow={'hidden'}>
-                    <Text as={'b'} fontSize={18} whiteSpace={'nowrap'}>Experience:</Text>
-
-                    <Text>{skillCard.experience}</Text>
-                  </Stack>
-
-                  <Stack direction={'row'} alignItems={'center'} justifyContent={'start'} spacing={2} overflow={'hidden'}>
-                    <Text as={'b'} fontSize={18} whiteSpace={'nowrap'}>Color:</Text>
-
-                    <Text>{skillCard.color}</Text>
-                  </Stack>
-                </Stack>
-              </CardBody>
-
-              <CardFooter p={4}>
-                <Stack direction={'row'} alignItems={'center'} justifyContent={'center'} spacing={4}>
-                  <Button variant={'solid'} colorScheme={'teal'}>Edit</Button>
-
-                  <Button variant={'solid'} colorScheme={'red'}>Remove</Button>
-                </Stack>
-              </CardFooter>
-            </Stack>
-          </Card>
-          ))
-        :
-          <Stack direction={'column'} alignItems={'center'} justifyContent={'center'} w={'full'} spacing={4}>
-            <Text>Skills list is empty</Text>
-
+    <>
+      <BaseContentHeaderContainer>
+        {
+          payload.length && <Stack direction={'row'} alignItems={'start'} justifyContent={'end'} w={'full'} spacing={4}>
             <Button colorScheme={'teal'} onClick={() => prepareCreateSkill()}>Create skill</Button>
+
+            <Button colorScheme={'red'}>Remove all skills</Button>
           </Stack>
-      }
-    </BaseContentContainer>
+        }
+      </BaseContentHeaderContainer>
+
+      <BaseContentContainer>
+        {
+          payload.length ?
+            payload.map((skillCard) => (
+              <Card
+                key={skillCard.id}
+                direction={{ base: 'column', sm: 'row' }}
+                overflow={'hidden'}
+                variant={'outline'}
+                w={'full'}>
+                <Stack direction={'column'} w={'full'} overflow={'hidden'}>
+                  <CardBody p={4}>
+                    <Stack direction={'column'} w={'full'} spacing={4} overflow={'hidden'}>
+                      <Stack direction={'row'} spacing={4}>
+                        <Tooltip label={'Skill visibility'} aria-label={'skill visibility'}>
+                          <Icon color={skillCard.visibility ? 'teal.500' : 'red.500'} as={skillCard.visibility ? VisibilityIcon : VisibilityOffIcon}/>
+                        </Tooltip>
+
+                        <Heading size={'md'}>{skillCard.title}</Heading>
+                      </Stack>
+
+                      <Stack direction={'row'} alignItems={'center'} justifyContent={'start'} spacing={2} overflow={'hidden'}>
+                        <Text as={'b'} fontSize={18} whiteSpace={'nowrap'}>Experience:</Text>
+
+                        <Text>{skillCard.experience}</Text>
+                      </Stack>
+
+                      <Stack direction={'row'} alignItems={'center'} justifyContent={'start'} spacing={2} overflow={'hidden'}>
+                        <Text as={'b'} fontSize={18} whiteSpace={'nowrap'}>Color:</Text>
+
+                        <Text>{skillCard.color}</Text>
+                      </Stack>
+                    </Stack>
+                  </CardBody>
+
+                  <CardFooter p={4}>
+                    <Stack direction={'row'} alignItems={'center'} justifyContent={'center'} spacing={4}>
+                      <Button variant={'solid'} colorScheme={'teal'}>Edit</Button>
+
+                      <Button variant={'solid'} colorScheme={'red'}>Remove</Button>
+                    </Stack>
+                  </CardFooter>
+                </Stack>
+              </Card>
+            ))
+            :
+            <Stack direction={'column'} alignItems={'center'} justifyContent={'center'} w={'full'} spacing={4}>
+              <Text>Skills list is empty</Text>
+
+              <Button colorScheme={'teal'} onClick={() => prepareCreateSkill()}>Create skill</Button>
+            </Stack>
+        }
+      </BaseContentContainer>
+    </>
   );
 };
 
