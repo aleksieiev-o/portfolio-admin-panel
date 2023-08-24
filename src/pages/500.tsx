1d01dfcbf770f45be425caffff1ebc71262da906
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2023-08-24 22:58:05 +0200

    edit go home route

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,19 +1,23 @@
-import { ReactElement } from 'react';
+import {ReactElement, useContext} from 'react';
 import { NextPage } from 'next';
 import SimpleLayout from '@/components/layout/SimpleLayout';
 import { useRouter } from 'next/router';
-import { ProtectedRoutePath } from '@/router/Routes.enum';
+import {ProtectedRoutePath, PublicRoutePath} from '@/router/Routes.enum';
 import { Button, Stack, Text } from '@chakra-ui/react';
+import {AuthContext} from '@/providers/AuthContext.provider';
 
 const ServerErrorPage: NextPage = (): ReactElement => {
   const router = useRouter();
+  const {currentUser} = useContext(AuthContext);
 
   return (
     <SimpleLayout title={'Internal server error'}>
       <Stack direction={'column'} alignItems={'center'} justifyContent={'center'} spacing={4} h={'full'} w={'full'}>
         <Text>Internal server error</Text>
 
-        <Button onClick={() => router.push(ProtectedRoutePath.PERSONAL_INFO)} boxShadow={'md'}>
+        <Button
+          onClick={() => router.push(currentUser ? ProtectedRoutePath.PERSONAL_INFO : PublicRoutePath.LOGIN)}
+          boxShadow={'md'}>
           Go home
         </Button>
       </Stack>
