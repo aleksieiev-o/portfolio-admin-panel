d96314f1af66c38e4e4da2d1c290c060d19963f2
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2023-09-05 23:10:30 +0200

    fix create skill payload

--- src/services/skills.service.ts
+++ src/services/skills.service.ts
@@ -10,13 +10,14 @@ export const fetchAllSkills = async (): Promise<Array<ISkill>> => {
 };
 
 export const createSkill = async (payload: TypeCreateSkillDto): Promise<void> => {
-  const {title, visibility, experience, color, position} = payload;
+  const {title, visibility, isMain, experience, color, position} = payload;
   const skillRef = push(ref(firebaseDataBase, EndpointsList.SKILLS));
 
   const skill: ISkill = {
     id: skillRef.key!,
     title,
     visibility,
+    isMain,
     experience,
     color,
     position: position,
